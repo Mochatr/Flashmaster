9c488675b657b9db5d1cc0ced64c9413e83c152e
Mochatr <mohamedchatr8@gmail.com>
2024-09-10 11:57:03 +0100

    Update: Initial style for dashboard and refactor the route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './components/Homepage';
 import LoginSignup from './components/LoginSignup/LoginSignup';
+import Dashboard from './components/Dashboard';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/Authenticate" element={<LoginSignup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
