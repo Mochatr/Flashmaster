868a417575f726947ef13de1865aa74109489b12
Mochatr <mohamedchatr8@gmail.com>
2024-09-10 23:36:54 +0100

    feat: Add LoginSignup component with styling

--- frontend/src/components/LoginSignup/LoginSignup.jsx
+++ frontend/src/components/LoginSignup/LoginSignup.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginSignup.css';
 
 import user_icon from '../assets/person.png';
@@ -6,17 +6,22 @@ import email_icon from '../assets/email.png';
 import password_icon from '../assets/password.png';
 
 const LoginSignup = () => {
+
+  const [action, setAction] = useState('Login'); 
+
+
     return (
       <div className="container">
         <div className="header">
-          <div className="text">Sign Up</div>
+          <div className="text">{action}</div>
           <div className="underline"></div>
         </div>
         <div className="inputs">
-          <div className="input">
+          {action==="Login"?<div></div>:<div className="input">
             <img src={user_icon} alt="" />
             <input type="text" placeholder="Name" />
-          </div>
+          </div>}
+
           <div className="input">
             <img src={email_icon} alt="" />
             <input type="email" placeholder="Email ID" />
@@ -26,12 +31,12 @@ const LoginSignup = () => {
             <input type="password" placeholder="Password" />
           </div>
         </div>
-        <div className="forgot-password">Lost your password? <span>Click Here!</span></div>
+        {action==="Sign Up"?<div></div>:<div className="forgot-password">Lost your password? <span>Click Here!</span></div>}
         <div className="submit-container">
-         <div className="submit">Sign Up</div>
-         <div className="submit">Login</div>
+         <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+         <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
         </div>
-    </div>
+     </div>
     );
 };
 
